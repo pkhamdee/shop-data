cf2241bdcb2afb4d7b38cc270fd9427654e5a5f1
Pongsak Khamdee <pongsakk@pongsakkPT69N.vmware.com>
2023-07-22 21:58:51 +0700

    add docker

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.enableCors();
 
   if (!configService.isProduction()) {
     const document = SwaggerModule.createDocument(app, new DocumentBuilder()
